458a39df7048572c3de267522d0450a4ec8eb503
yrahan <yacine.bourkeb@google.com>
2019-09-16 17:58:59 +0200

    setting up admin page

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=20)
+    birth_date = models.DateTimeField('birth date')
+
+    def is_underage(self):
+        return timezone.now().year - self.birth_date.year < 18
+
+    def __str__(self):
+        return self.name
+
+class Bicycle(models.Model):
+    client = models.ForeignKey(Client, on_delete = models.CASCADE)
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+    
