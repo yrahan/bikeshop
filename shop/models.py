9e4c44f2c7c992da6a556cb5314e1cd2175cbab2
yrahan <yacine.bourkeb@google.com>
2019-10-06 15:01:03 +0200

    local copy

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.db import models
 from django.utils import timezone
 
@@ -7,8 +5,9 @@ from django.utils import timezone
 # Create your models here.
 
 class Client(models.Model):
-    name = models.CharField(max_length=20)
-    birth_date = models.DateTimeField('birth date')
+    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
 
     def is_underage(self):
         return timezone.now().year - self.birth_date.year < 18
@@ -16,9 +15,12 @@ class Client(models.Model):
     def __str__(self):
         return self.name
 
+
 class Bicycle(models.Model):
-    client = models.ForeignKey(Client, on_delete = models.CASCADE)
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
+    brand = models.CharField(max_length=20)
+    to_fix = models.CharField(max_length=20)
+
     def __str__(self):
         return self.name
-    
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,21 @@
-#from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
+from django.http import Http404
 from django.http import HttpResponse
 
+
+from .models import Client
+
 def index(request):
-    return HttpResponse("Hello, Mimmi!<br>You're at the awesome Bikeshop homepage.")
+    latest_client_list=Client.objects.order_by('-birth_date')[:5]
+    context = {
+            'latest_client_list' : latest_client_list,
+            }
+    return render(request, 'shop/index.html', context )
+
+def detail(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+    return render(request, "shop/detail.html", {client : "client"})
+
+
